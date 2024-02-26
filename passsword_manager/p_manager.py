462c16f0dc26429700a07722fd47b05410d8d93d
unknown <sawdickagboke@gmail.com>
2024-02-26 10:53:40 +0100

    proj

---
+++ passsword_manager/p_manager.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from random import choice, randint, shuffle
+from tkinter import messagebox
+import pyperclip
+
+
+
+
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1','2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = [',', '@', '*', '$', '^', '=', '-']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2,4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showerror(title="oops", message="fill in all fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"these are the details entered \nemail, \npassword, \nwebsite")
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"{website} | {email} | {password} \n")
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+website_label = Label(text="website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email: ")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password: ")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=36)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=35)
+password_entry.grid(row=3, column=1)
+
+genarate_button = Button(text="Generate Password", command=generate_password)
+genarate_button.grid(row=3, column=2)
+
+add_button = Button(text="add", command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+window.mainloop()
